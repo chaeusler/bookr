1c1bd04f0afa846e26d49f73fc158bc3d5422c9e
Christian Häusler <christian.haeusler@prodyna.com>
2015-03-30 08:02:42 +0200

    added first controller

--- AngularClient/src/app/index.js
+++ AngularClient/src/app/index.js
@@ -1,14 +1,18 @@
 'use strict';
 
 angular.module('bookr', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute', 'ui.bootstrap'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/main', {
+                templateUrl: 'app/main/main.html',
+                controller: 'MainCtrl'
+            })
+            .when('/persons', {
+                templateUrl: 'app/main/persons.html',
+                controller: 'PersonsCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
 ;
--- AngularClient/src/components/navbar/navbar.controller.js
+++ AngularClient/src/components/navbar/navbar.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 angular.module('bookr')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
+    .controller('NavbarCtrl', function ($scope) {
+        $scope.pages = [
+            {
+                href: '#main',
+                title: 'Home'
+            },
+            {
+                href: '#persons',
+                title: 'Persons'
+            }];
+
+
+        $scope.activeHref = '/#/main.html';
+
+        $scope.date = new Date();
+    });
