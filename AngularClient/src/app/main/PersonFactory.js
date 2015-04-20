6fe4cbb9d7a11762607dcb5690742713012f26b6
Christian Häusler <ch.haeusler@gmail.com>
2015-04-20 21:26:02 +0200

    update of person

--- AngularClient/src/app/main/PersonFactory.js
+++ AngularClient/src/app/main/PersonFactory.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('bookr').factory('Person', ['$resource', function ($resource) {
-    return $resource('http://localhost:8080/bookr/rest/v1/persons/:userId', {}, {
+    return $resource('http://localhost:8080/bookr/rest/v1/persons/:userId', {userId:'@id'}, {
         query: {
             method: 'GET',
             headers: {Authorization: 'Basic ' + btoa('user' + ':' + 'user')},
             isArray: true
+        },
+        update: {
+            method: 'PUT',
+            headers: {Authorization: 'Basic ' + btoa('user' + ':' + 'user')}
         }
     });
 }]);
--- AngularClient/src/app/main/personsController.js
+++ AngularClient/src/app/main/personsController.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('bookr').controller('PersonsCtrl', ['$scope', 'Person', function ($scope, Person) {
-        $scope.persons = Person.query();
-        $scope.selected = {};
-
+    $scope.persons = Person.query();
+    $scope.update = function (person) {
+        Person.update(person);
+    };
 }]);
